3b6cfedd3f11968af55b30dcab36529463b1912b
EzekielCz <ezechielcazacu@gmail.com>
2024-04-18 20:08:32 -0500

    configuring data to be sent to back end

--- beatbuzz-ui/client/src/App.js
+++ beatbuzz-ui/client/src/App.js
@@ -1,25 +1,69 @@
 
-import React, {useEffect, useState} from "react";
-import Axios from "axios";
-=======
+// import React, {useEffect, useState} from "react";
+// import Axios from "axios";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, FormControl, Button, Row, Card, CardBody } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-import Dashboard from './Dashbord';
-import Login from './Login';
 
-const getData=async()=>{
-  const response=await Axios.get("http://localhost:8080/api/hello");
-  setData(response.data);
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Container, InputGroup, FormControl, Button, Row, Card, CardBody } from 'react-bootstrap';
+// import { useState, useEffect } from 'react';
+// import Dashboard from './Dashbord';
+// import Login from './Login';
+
+// const getData=async()=>{
+//   const response=await Axios.get("http://localhost:8080/api/hello");
+//   setData(response.data);
+// }
+
+// useEffect(()=>{
+//   getData()
+
+// },[]);
+// return(
+//   <div>{data}</div>
+// )
+// }
+
+
+// import Register from "./Register";
+// import React, {useEffect, useState} from "react";
+// import Axios from "axios";
+// const App = () => {
+// const [data, setData]=useState("");
+// const getData=async()=>{
+//   const response=await Axios.get("http://localhost:8080/api/hello");
+//   setData(response.data);
+// }
+// useEffect(()=>{
+//   getData()
+// },[]);
+// return(
+//   <div>{data}</div>
+// )
+// }
+
+// function App() {
+
+//   return (
+//     <Register />
+//   )
+// }
+// export default App
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Login from "./Login";
+import Register from "./Register";
+// import CreatePostForm from "./CreatePostForm";
+const code = new URLSearchParams(window.location.search).get("code");
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        {/* <Route path="/post/create" element={<CreatePostForm />} /> */}
+      </Routes>
+    </Router>
+  );
 }
-
-useEffect(()=>{
-  getData()
-
-},[]);
-return(
-  <div>{data}</div>
-)
-}
-export default App
+export default App;
